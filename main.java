67822521bcde72f798fe78d2f353fd336a1e2b85
Aleksandr-study <16bonya2002@gmail.com>
2022-06-22 22:23:46 +0300

    Modernization

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
 class CaesarCipher {
-    // Encrypt
-    public static StringBuffer encrypt(String text, int count) {
+    public static StringBuffer crypt(String text, int count) {
         StringBuffer result = new StringBuffer();
         for (int i = 0; i < text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) {
@@ -11,33 +10,17 @@ class CaesarCipher {
                 result.append(ch);
             }
         }
-        return result;
-    }
-
-    // Decrypt
-    public static StringBuffer decrypt(String cipher, int count) {
-        StringBuffer result = new StringBuffer();
-        for (int i = 0; i < cipher.length(); i++) {
-            if (Character.isUpperCase(cipher.charAt(i))) {
-                char ch = (char) (((int) cipher.charAt(i) + count - 65) % 26 + 65);
-                result.append(ch);
-            } else {
-                char ch = (char) (((int) cipher.charAt(i) + count - 97) % 26 + 97);
-                result.append(ch);
-            }
-        }
-        return result;
+        return result; 
     }
 
     public static void main(String[] args) {
-        String originalText = "HelloWorld";
-        int shift = 7;
+        String originalText = "Hello World";
+        int key = 5;
         System.out.println("Encryption:\nText  : " + originalText);
-        System.out.println("Shift : " + shift);
-        String cipher = encrypt(originalText, shift).toString();
+        System.out.println("Key: " + key);
+        String cipher = crypt(originalText, key).toString();
         System.out.println("Encrypted: " + cipher);
-        System.out.println("Shift : " + shift);
-        String decryptedPlainText = decrypt(cipher, 26 - shift).toString();
+        String decryptedPlainText = crypt(cipher, 26 - key).toString();
         System.out.println("Decrypted Text: " + decryptedPlainText);
     }
 }
